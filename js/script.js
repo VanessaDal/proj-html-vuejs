e62f00f6f3110e2fdb597d900ebfbba52863de54
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-12-10 16:47:52 +0100

    WIP nav with Vue

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var app=new Vue({
+    el: '#app',
+    data:{
+        nav:[{
+                nome:"Home",
+                url:"http://www.asnlsdk.it"
+            },
+        
+            {
+                nome:"Recipes",
+                url:"http://www.asnlsdk.it"
+             },
+    
+            {
+                nome:"Places",
+                url:"http://www.asnlsdk.it"
+             },
+
+            {
+                nome:"Blog",
+                url:"http://www.asnlsdk.it"
+             },
+
+            {
+    
+                nome:"Places",
+                url:"http://www.asnlsdk.it"
+            
+            },
+
+            {
+                nome:"Contact",
+                url:"http://www.asnlsdk.it"
+            },
+
+            {
+                nome:"Contact",
+                url:"http://www.asnlsdk.it"
+            }]}
+})
